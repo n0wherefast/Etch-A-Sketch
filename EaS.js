7ba868e23592f4004bf2b12647c11da7f6012501
Alessio <16mmstudios@gmail.com>
2022-03-30 16:28:08 +0200

    EaS.js index.html style.css

--- EaS.js
+++ EaS.js
@@ -1,127 +1,106 @@
-const big = document.querySelector('.big')
+const big = document.querySelector('.big');
 const container = document.querySelector('.container');
 const cleanBtn = document.querySelector('#clean');
-const lowBtn = document.querySelector('#lowres')
-const midBtn = document.querySelector('#midres')
-const highBtn = document.querySelector('#highres')
-const color = document.querySelector('#color')
-
+const lowBtn = document.querySelector('#lowres');
+const midBtn = document.querySelector('#midres');
+const highBtn = document.querySelector('#highres');
+const white = document.querySelector('#white');
+const black = document.querySelector('#black');
+const rainBow = document.querySelector('#rainbow');
+const eraser = document.querySelector('#eraser');
+let size ="";
+const cont = 640;
+let colo = '';
 
 lowBtn.addEventListener('click' , () =>{
-    container.innerHTML ="";  
-    theGrid(16) 
+    container.innerHTML="";
+    grid(16) ;
     
-}) 
+    
+}) ;
 midBtn.addEventListener('click' , (e) =>{
-    container.innerHTML =""; 
-
-    theGrid(32,)    
-    e.target.style.background='red'
-})
+    container.innerHTML="";
+    grid(32);
+});
 
 highBtn.addEventListener('click' , (e) =>{
     container.innerHTML ="";  
-    theGrid(64)
-    e.target.style.background='red' 
+    grid(64)  ;
 })
+white.addEventListener('click',() => {
+    container.addEventListener('mouseover',()=>{
+         colo ='white';
+ console.log(colo) 
+      })
+ 
+});
 
-color.addEventListener('click',() => {
- theGrid(32,1)
-})
-
-
-
-
-function theGrid(size,color){
+eraser.addEventListener('click',()=>{
+    container.addEventListener('mouseover',()=>{
+        colo ='rgb(179, 176, 176';
+    console.log(colo)
+      })
     
-  for(i=0;i<size*size;i++){
+});
 
-    const pixel =  document.createElement('grid');
-    container.appendChild(pixel);
+
+black.addEventListener('click',()=>{
+    container.addEventListener('mouseover',()=>{
+        colo ='black';
+    console.log(colo)
+      })
     
-     if(size===16){
-        
-      pixel.setAttribute('class','gridLow');
+});
 
-      pixel.addEventListener('mousemove', () =>{
 
-           if(color===1){
-            pixel.setAttribute('class','gridWhitelow1' ) 
-           }else{pixel.setAttribute('class','gridLow1' )}
-             
-        })
-        cleanBtn.addEventListener('click',(e) =>{
-        pixel.setAttribute('class','clean')
-         e.target.style.background='red'
-                 }) 
-                    
-                
-                   
-     }else if(size===32){
+rainBow.addEventListener('click',()=>{
+    container.addEventListener('mouseover',()=>{
+      colo = rgb()
+    console.log(colo)  
+    })
+    
 
-        pixel.setAttribute('class','gridMid')
+});
 
-        pixel.addEventListener('mouseover', () =>{
+function rgb(){
 
-            if(color===1){
-            pixel.setAttribute('class','gridWhiteMid1' ) 
-            }else{pixel.setAttribute('class','gridMid1' )}
-        }) 
-
-        cleanBtn.addEventListener('click',(e) =>{
-        pixel.setAttribute('class','clean')
-        e.target.style.background='red'
-            }) 
-        
-     }else if(size===64){
-
-        pixel.setAttribute('class','gridHigh')
-        pixel.addEventListener('mouseover', () =>{
-        pixel.setAttribute('class','gridHigh1' )
-       }) 
-       cleanBtn.addEventListener('click',(e) =>{
-       pixel.setAttribute('class','clean')
-       e.target.style.background='red'
-        }) 
-     }
-  }
+    let r =Math.random()*256;
+    let g =Math.random()*256;
+    let b =Math.random()*256;
+    colo = `rgb(${r}, ${g} ,${b} )`; 
+    return colo;
 }
 
-/*
-function theGridWhite(size){
+
+
+
+function grid(size,){ 
+
+  let value = cont / size;
+
+  for(i=0;i<size*size;i++){
+     
+    const pixel =  document.createElement('grid');
+    container.appendChild(pixel);
+    pixel.style.display ='flex';
+    pixel.style.backgroundColor='rgb(179, 176, 176';
+    pixel.style.height= `${value}px`;
+    pixel.style.width= `${value}px`;
+
     
-    for(i=0;i<size*size;i++){
-  
-      const pixel =  document.createElement('grid')
-      container.appendChild(pixel)
-      
-       if(size===16)
-          
-          pixel.setAttribute('class','gridLow')
 
-          pixel.addEventListener('mouseover', () =>{
+           pixel.addEventListener('mouseover',(e)=>{
+           
+           e.target.style.backgroundColor= `${colo}`
+    });
 
-             pixel.setAttribute('class','gridWhitelow1' )
-          }) 
-                cleanBtn.addEventListener('click',(e) =>{
-                     
-                   pixel.setAttribute('class','clean')
-                    e.target.style.background='red'
-                 })
+    cleanBtn.addEventListener('click',()=>{ 
 
-        }else if(size===32){
-  
-          pixel.setAttribute('class','gridMid')
-          pixel.addEventListener('mouseover', () =>{
-          pixel.setAttribute('class','gridMid1' )
-          }) 
-       }else if(size===64){
-  
-          pixel.setAttribute('class','gridHigh')
-          pixel.addEventListener('mouseover', () =>{
-          pixel.setAttribute('class','gridHigh1' )
-         }) 
-       }*/
+             pixel.style.display='none';
+    });
+
+  };
+};
     
   
   
